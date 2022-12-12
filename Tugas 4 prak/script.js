a4426d904fef89bd3c213722c46ff38e39b62376
Pandu160402 <83668910+Pandu160402@users.noreply.github.com>
2022-12-12 19:27:43 +0700

    Add files via upload

---
+++ Tugas 4 prak/script.js
@@ -0,0 +1,11 @@
+
+// function tekan (){
+//     var press= document.getElementById("press");
+//     press.document.body.classlist.toggle("darkmode")
+// }
+
+var tombol = document.getElementById("tombol")
+
+tombol.onclick = function (){
+    document.body.classList.toggle("darkmode");
+}
